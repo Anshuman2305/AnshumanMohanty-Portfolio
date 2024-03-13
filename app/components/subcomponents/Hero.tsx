6ce4775b5f333e381b83b085f86ca83351847089
Anshuman2305 <anshuman.nitrr@gmail.com>
2024-03-13 12:18:19 +0530

    Minor UI Change

--- app/components/subcomponents/Hero.tsx
+++ app/components/subcomponents/Hero.tsx
@@ -9,7 +9,7 @@ function hero() {
           <h1 className="text-4xl md:text-5xl text-brightgreen font-bold">Anshuman <br/> Mohanty</h1>
           <p className="text-base text-mediumgreen">Digging for  <span className="bg-brightgreen px-2 text-darkgreen-bg">ore</span> by day, digging into  <span className="bg-brightgreen px-2 text-darkgreen-bg">code</span>  by night.</p>
           <p className="text-base text-justify text-mediumgreen max-w-lg ">From delving into the depths of the earth to navigating the vast expanses of the digital realm, I am a mining student with a passion for both extracting ore and crafting code.</p>
-          <a href="/ref" className="md:animate-bounce transition ease-in-out duration-300 bg-dullgreen hover:bg-brightgreen hover:text-darkgreen-bg text-brightgreen md:w-1/4 text-center py-2 lg:p-2 rounded-lg md:mt-4">Resume</a>
+          <a href="https://drive.google.com/file/d/1Oyf4DSwglNwLsweQ2_NyNP9RH02-qlpH/view?usp=drive_link" target='blank' className="md:animate-bounce transition ease-in-out duration-300 bg-dullgreen hover:bg-brightgreen hover:text-darkgreen-bg text-brightgreen md:w-1/4 text-center py-2 lg:p-2 rounded-lg md:mt-4">Resume</a>
         </div>
         <Image src={"/photos/photo.jpeg"} width={200} height={200} alt="anshuman mohanty" className="rounded-xl lg:hidden xl:block md:w-64 md:h-80" />
     </div>
--- app/components/subcomponents/reusableComponents/Projectcard.tsx
+++ app/components/subcomponents/reusableComponents/Projectcard.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 type ProjectCardProps = {
   title: string;
   description: string;
+  web_link: string;
 };
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ title, description }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, web_link }) => {
   return (
-    <div className='card flex basis-1/2 flex-col gap-2 justify-center border-[2px] rounded-2xl border-dullgreen px-2 py-4 md:p-4'>
+    <a href={web_link} target='blank' className='card transition ease-in-out duration-300 hover:bg-dullgreen flex basis-1/2 flex-col gap-2 justify-center border-[2px] rounded-2xl border-dullgreen px-2 py-4 md:p-4'>
       <h2 className='text-brightgreen text-[16px] md:text-lg lg:text-xl px-2 font-bold line-clamp-1'>{title}</h2>
       <p className='text-mediumgreen text-[14px] md:text-sm px-2 text-justify'>{description}</p>
-    </div>
+    </a>
   );
 };
 
